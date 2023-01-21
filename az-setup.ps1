bbb8478b6c2786574a70000c664523b2e3f76820
Bill Melvin <bill@billmelvin.com>
2023-01-21 16:44:37 -0500

    Update az-setup.ps1.

--- az-setup.ps1
+++ az-setup.ps1
@@ -2,16 +2,14 @@
 #  PowerShell script with steps to deploy using the Azure CLI.
 #
 #  Some commented-out commands in this script are intended to be executed
-#  separately using F8 in an IDE (or by pasting into a shell to run).
+#  separately using F8 in an IDE (or by pasting into a shell).
 #
 #  Running this script will:
 #  - Create a Resource Group.
+#  TODO: Fill in the missing items here...
 #  - Create an App Service Plan (Linux).
 #  - Create a Web App.
 #  - Configure the Web App for ZIP file deployment.
-#  - Create SQL Server.
-#  - Add firewall rules to the SQL Server.
-#  - Create a Database on the SQL Server.
 #
 # ----------------------------------------------------------------------
 
@@ -23,13 +21,10 @@
 # Script initialization:
 
 $baseName = "fileupaz"
-
-$doSQLServer = $true
+$uniqtag = "21"
 
 
 # -- Get key variables from file in local encrypted folder.
-
-# $keysFile = "$env:UserProfile\KeepLocal\${baseName}-settings.ps1"
 
 $profilePath = [Environment]::GetFolderPath([Environment+SpecialFolder]::UserProfile)
 $keysFile = [System.IO.Path]::Combine($profilePath, "KeepLocal", "${baseName}-settings.ps1")
@@ -48,8 +43,8 @@ function CheckVarSet ([string] $varName) {
 }
 
 CheckVarSet "fileupSettings"
-CheckVarSet "sqlAdminUser"
-CheckVarSet "sqlAdminPass"
+CheckVarSet "storageRoleAssignee"
+
 
 # -- Check that required settings (dictionary keys) exist.
 function CheckKeyExists ([string] $varName) {
@@ -72,35 +67,28 @@ CheckKeyExists "FILEUP_MSAL_SCOPE"
 CheckKeyExists "FILEUP_STORAGE_ACCOUNT_URL"
 CheckKeyExists "FILEUP_STORAGE_CONNECTION"
 CheckKeyExists "FILEUP_STORAGE_CONTAINER"
+CheckKeyExists "FILEUP_TABLES_CONNECTION"
 
 
 # -- Assign additional variables used in this script.
 
-$rgName = "${baseName}-rg"
+$rgName = "${baseName}_rg"
 $location = "eastus"
-$uniqtag = "31"
 $appServiceName = "${baseName}${uniqtag}appserv"
 $webAppName = "${baseName}${uniqtag}webapp"
-# $appInsightsName = "${baseName}${uniqtag}insights"
-$sqlServerName = "${baseName}${uniqtag}dbsrv"
-$sqlDatabaseName = "${baseName}${uniqtag}sqldb"
+$storageAcctName = "${baseName}${uniqtag}storage"
+
+$storageContainerName = $fileupSettings["FILEUP_STORAGE_CONTAINER"]
+if (!$storageContainerName) {
+  $storageContainerName = "fileup"
+}
+Write-Host "INFO: storageContainerName = '$storageContainerName'"
+
 
 #  Build a settings string, to use in 'az webapp config appsettings', using the
 #  key=value pairs in the $fileupSettings dictionary loaded from $keysFile.
 
-if ($doSQLServer) {
-  $escpw = [uri]::EscapeDataString($SqlAdminPass)
-
-  $appDatabaseURI = "mssql+pyodbc://${sqlAdminUser}:${escpw}@${sqlServerName}.database.windows.net:1433/${sqlDatabaseName}?driver=ODBC+Driver+18+for+SQL+Server"
-  
-  
-  $appSettingsStr = ('"FILEUP_DATABASE_URI=' + $appDatabaseURI + '"')
-}
-else {
-  $appSettingsStr = ""
-}
-
-
+$appSettingsStr = ""
 foreach ($key in $fileupSettings.Keys) {
   $value = $fileupSettings[$key]
   if (0 -lt $value.Length) {
@@ -115,24 +103,89 @@ foreach ($key in $fileupSettings.Keys) {
 # ======================================================================
 # Create and configure Azure resources.
 
-Write-Host "`nSTEP - Creating resource group: $rgName`n"
+Write-Host "`nSTEP - Create resource group: $rgName`n"
 
 az group create -n $rgName -l $location
 
 
-# SKIP
-# # -- Create the Application Insights resource.
-# #    https://docs.microsoft.com/en-us/cli/azure/resource?view=azure-cli-latest#az-resource-create
+Write-Host "`nSTEP - Create Storage Account: $storageAcctName`n"
 
-# az resource create -n $appInsightsName -g $rgName `
-#   --resource-type "Microsoft.Insights/components" `
-#   --properties '{\"Application_Type\":\"web\"}'
+# -- Create the Storage Account.
+#    https://docs.microsoft.com/en-us/cli/azure/storage/account?view=azure-cli-latest#az-storage-account-create
+
+az storage account create -n $storageAcctName -l $location -g $rgName --sku Standard_LRS
+
+
+# -- Get the storage account key.
+#    Example found in Microsoft Docs: "Mount a file share to a Python function app - Azure CLI"
+#    https://docs.microsoft.com/en-us/azure/azure-functions/scripts/functions-cli-mount-files-storage-linux
+
+$storageKey = $(az storage account keys list -g $rgName -n $storageAcctName --query '[0].value' -o tsv)
+
+
+# -- Get the connection string.
+#    https://learn.microsoft.com/en-us/cli/azure/storage/account?view=azure-cli-latest#az-storage-account-show-connection-string-examples
+
+$storageConnStr = $(az storage account show-connection-string -g $rgName -n $storageAcctName --query connectionString --output tsv)
+
+
+
+# -- Assign role to access blob storage. Used for DefaultAzureCredential to
+#    access BlobServiceClient when not using a connection string.
+#
+#    Authenticate to Azure and authorize access to blob data:
+#    https://learn.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-dotnet?tabs=visual-studio%2Cmanaged-identity%2Croles-azure-cli%2Csign-in-azure-cli%2Cidentity-visual-studio#authenticate-to-azure-and-authorize-access-to-blob-data
+
+if ($storageRoleAssignee) {
+  Write-Host "`nSTEP - Assign blob data access role for : $storageAcctName`n"
+  $storageResourceId = $(az storage account show -g $rgName -n $storageAcctName --query id)
+  az role assignment create --assignee $storageRoleAssignee --role "Storage Blob Data Contributor" --scope $storageResourceId
+
+  #  If the setting for FILEUP_STORAGE_ACCOUNT_URL is empty, it will not be
+  #  in $appSettingsStr. Add that setting when using $storageRoleAssignee.
+
+  if (!$appSettingsStr.Contains("FILEUP_STORAGE_ACCOUNT_URL")) {
+    Write-Host "`nAdd setting FILEUP_STORAGE_ACCOUNT_URL`n"
+    $storageAcctUrl = "https://${storageAcctName}.blob.core.windows.net"
+    $s = '"FILEUP_STORAGE_ACCOUNT_URL=' + $storageAcctUrl + '"'
+    $appSettingsStr += " $s"
+  }  
+}
+else {
+  #  When not using the DefaultAzureCredential to access blob storage,
+  #  set the connection string.
+  if (!$appSettingsStr.Contains("FILEUP_STORAGE_CONNECTION")) {
+    Write-Host "`nAdd setting FILEUP_STORAGE_CONNECTION`n"
+    $s = '"FILEUP_STORAGE_CONNECTION=' + $storageConnStr + '"'
+    $appSettingsStr += " $s"
+  }
+}
+
+
+# -- Add setting for table storage connection.
+
+if (!$appSettingsStr.Contains("FILEUP_TABLES_CONNECTION")) {
+  Write-Host "`nAdd setting FILEUP_TABLES_CONNECTION`n"
+  $s = '"FILEUP_TABLES_CONNECTION=' + $storageConnStr + '"'
+  $appSettingsStr += " $s"
+}
+
+
+# -- Create storage container.
+#    https://docs.microsoft.com/en-us/cli/azure/storage/container?view=azure-cli-latest#az-storage-container-create
+
+Write-Host "`nSTEP - Create Storage Container: $storageContainerName`n"
+
+az storage container create `
+  --account-key $storageKey `
+  --account-name $storageAcctName `
+  --name $storageContainerName
 
 
 # -- Create the App Service Plan (Linux).
 #    https://docs.microsoft.com/en-us/cli/azure/appservice/plan?view=azure-cli-latest#az-appservice-plan-create
 
-Write-Host "`nSTEP - Creating App Service Plan: $appServiceName`n"
+Write-Host "`nSTEP - Create App Service Plan: $appServiceName`n"
 
 az appservice plan create `
   --name $appServiceName `
@@ -146,7 +199,7 @@ az appservice plan create `
 #
 #    az webapp list-runtimes
 
-Write-Host "`nSTEP - Creating Web App: $webAppName`n"
+Write-Host "`nSTEP - Create Web App: $webAppName`n"
 
 az webapp create `
   -g $rgName `
@@ -158,7 +211,7 @@ az webapp create `
 # -- Configure for ZIP file deployment.
 #    https://learn.microsoft.com/en-us/azure/app-service/quickstart-python?tabs=flask%2Cwindows%2Cazure-cli%2Czip-deploy%2Cdeploy-instructions-azcli%2Cterminal-bash%2Cdeploy-instructions-zip-azcli#3---deploy-your-application-code-to-azure
 
-Write-Host "`nSTEP - Configuring settings for: $webAppName`n"
+Write-Host "`nSTEP - Configure settings for: $webAppName`n"
 
 az webapp config appsettings set `
     -g $rgName `
@@ -169,7 +222,8 @@ az webapp config appsettings set `
 # -- Configure settings for the web app. These are available to the app as environment variables.
 #    https://learn.microsoft.com/en-us/cli/azure/webapp/config/appsettings?view=azure-cli-latest
 
-Write-Host "`nSTEP - Configuring web app settings for: $webAppName`n"
+Write-Host "`nSTEP - Configure web app settings for: $webAppName`n"
+
 
 #  In order to treat the settings in $appSettingsStr as separate arguments that
 #  follow '--settings', create the az command as an expression and invoke it.
@@ -177,43 +231,6 @@ Write-Host "`nSTEP - Configuring web app settings for: $webAppName`n"
 $expr = "az webapp config appsettings set -g $rgName --name $webAppName --settings $appSettingsStr"
 Invoke-Expression $expr
 
-
-if ($doSQLServer) {
-  # -- Create SQL Server.
-  #    https://docs.microsoft.com/en-us/cli/azure/sql/server?view=azure-cli-latest#az-sql-server-create
-
-  Write-Host "`nSTEP - Creating SQL Server: $sqlServerName`n"
-
-  az sql server create --name $sqlServerName -g $rgName `
-    --location $location `
-    --admin-password $SqlAdminPass `
-    --admin-user $SqlAdminUser
-
-
-  # -- Add firewall rules for the web app's outbound IP addresses to the SQL Server.
-  #    https://docs.microsoft.com/en-us/azure/app-service/overview-inbound-outbound-ips#find-outbound-ips
-  #    https://docs.microsoft.com/en-us/cli/azure/sql/server/firewall-rule?view=azure-cli-latest#az-sql-server-firewall-rule-create
-
-  Write-Host "`nSTEP - Adding firewall rules for: $sqlServerName`n"
-
-  $ipAddresses = az webapp show -g $rgName --name $webAppName --query outboundIpAddresses --output tsv
-  $ipNum = 0
-  foreach ($ip in $ipAddresses.Split(","))
-  {
-    $ipNum += 1
-    $ruleName = "WebAppIP$ipNum"
-    Write-Host $ruleName $ip
-    az sql server firewall-rule create -g $rgName -s $sqlServerName -n $ruleName --start-ip-address $ip --end-ip-address $ip
-  }
-
-
-  # -- Create SQL Database.
-  #    https://docs.microsoft.com/en-us/cli/azure/sql/db?view=azure-cli-latest#az-sql-db-create
-
-  Write-Host "`nSTEP - Creating database: $sqlDatabaseName`n"
-
-  az sql db create --name $sqlDatabaseName -g $rgName --server $sqlServerName
-}
 
 # -- Set custom startup command for running the Flask app.
 #    https://learn.microsoft.com/en-us/azure/app-service/configure-language-python#customize-startup-command
@@ -227,14 +244,6 @@ az webapp config set -g $rgName --name $webAppName --startup-file $startCmd
 # ----------------------------------------------------------------------
 # Additional commands and information.
 
-# -- To add the local IP to the Database Server in the Azure Portal:
-#    - Select the Resource Group.
-#    - Select the SQL Server.
-#    - Select the Networking blade.
-#    - Scroll to Firewall Rules.
-#    - Select 'Add your client IPv4 address'.
-#    - Click 'Save'.
-
 
 # -- Zip deploy (update $zipFile value before running).
 #    https://learn.microsoft.com/en-us/cli/azure/webapp?view=azure-cli-latest#az-webapp-deploy
@@ -243,16 +252,9 @@ az webapp config set -g $rgName --name $webAppName --startup-file $startCmd
 # az webapp deploy --name $webAppName -g $rgName --src-path $zipFile
 
 
-# -- Get the database connection string. The Flask app does not use this.
-#
-# $connStr = $(az sql db show-connection-string -s $sqlServerName -n $sqlDatabaseName -c ado.net)
-
-
 # -- List resources.
 #
 # az resource list -g $rgName -o table
 
 
-# -- Delete the whole lot when done.
-#
-# az group delete -n $rgName
+# ======================================================================
